8bde1dd166144800630f952d39c9f2b955c5ca56
Jarrod Swift <jarrod.swift@sustainabilityhouse.com.au>
2011-05-13 01:00:33 +0000

    Updated docs ready to tag 0.3
    git-svn-id: https://flexible-orm.googlecode.com/svn/trunk@45 d54b65da-0f49-d961-2ead-5ae8ea9d2a07

--- Interfaces/DataStatement.php
+++ Interfaces/DataStatement.php
@@ -11,14 +11,50 @@ namespace ORM\Interfaces;
  * @see DataFactory::Get()
  * @author jarrod.swift
  *
- * @todo Document the required methods
  */
 interface DataStatement {
+    /**
+     * Execute the prepared statement
+     * 
+     * @see http://www.php.net/manual/en/pdostatement.execute.php
+     */
     public function execute( $values );
+    
+    /**
+     * Bind a variable to a placeholder
+     * 
+     * @see http://www.php.net/manual/en/pdostatement.bindparam.php
+     */
     public function bindParam( $placeholder, &$value );
+    
+    /**
+     * Bind a value to a placeholder
+     */
     public function bindValue( $placeholder, $param );
-    public function bindObject( $className, array $params );
+    
+    /**
+     * Bind all placeholders to the corresponding object properties
+     * 
+     * e.g.
+     * @code
+     * UPDATE car SET doors = :doors, name = :name
+     * @endcode
+     * 
+     * Would attempt to bind \c $object->doors to :doors and \c $object->name to
+     * :name
+     */
+    public function bindObject( $object, array $params );
+    
+    /**
+     * Fetch the results of the query in to an object of this classname
+     */
     public function fetchInto( $className );
+    
+    /**
+     * Fetch all rows returned as instances of class $className
+     * 
+     * @return ModelCollection
+     */
     public function fetchAllInto( $className );
 }
 ?>
