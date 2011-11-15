3f51b2611404570f1da691a6d6bbce79a97ecdbd
Jarrod Swift <jarrod.swift@sustainabilityhouse.com.au>
2011-11-15 21:03:16 +0000

    Added ErrorHandlerTest and passed initial tests of error_reporting
    git-svn-id: https://flexible-orm.googlecode.com/svn/branches/error-handler@269 d54b65da-0f49-d961-2ead-5ae8ea9d2a07

--- Utilities/ErrorHandler.php
+++ Utilities/ErrorHandler.php
@@ -11,5 +11,28 @@ namespace ORM\Utilities;
  *
  */
 class ErrorHandler {
+    
+    public function registerErrorHandler() {
+        
+    }
+    
+    public function registerShutdownHandler() {
+        
+    }
+    
+    public function handleError( $errorType, $errorMessage, $file, $line, array $context = array()) {
+        
+    }
+    
+    /**
+     * Determine whether an error of the supplied type would be displayed with
+     * the current error_reporting settings
+     * 
+     * @param int $errorType 
+     * @return boolean
+     */
+    public function displayErrorOfType( $errorType ) {
+        return (error_reporting() & $errorType) == true;
+    }
 }
 
