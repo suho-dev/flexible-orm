7c10e482353a031815fbb5cb86bdcd93cb5bc77a
Jarrod Swift <jarrod.swift@sustainabilityhouse.com.au>
2012-06-25 05:05:29 +0000

    Removed unused line
    git-svn-id: https://flexible-orm.googlecode.com/svn/trunk@407 d54b65da-0f49-d961-2ead-5ae8ea9d2a07

--- ORM_PDOStatement.php
+++ ORM_PDOStatement.php
@@ -100,7 +100,6 @@ class ORM_PDOStatement extends \PDOStatement implements Interfaces\DataStatement
     public function fetchAllInto( $className ) {
         $qualifiedColumnNames = $this->_getQualifiedColumnNames();
         $objects    = new ModelCollection();
-        $class      = basename(str_replace('\\', '/', $className));
 
         while ( $object = $this->_getObject($className, $qualifiedColumnNames) ) {
             $objects[] = $object;
